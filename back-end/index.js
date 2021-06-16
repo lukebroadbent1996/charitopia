577c4c45f158ded281164a262b4a37cb3874d7dc
Luke <lukebroadbent1996@gmail.com>
2021-06-16 14:21:11 +0100

    added password hashing

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,36 @@
-const express = require('express')
-const mysql = require('mysql')
-const app = express()
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const { response } = require('express');
 
-app.use(express.json())
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({
+    key: "userId",
+    secret: "superdupertopsecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60*60*24*1000,
+    } 
+}))
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -13,19 +41,26 @@ const db = mysql.createConnection({
 
 app.post('/register', (req, res)=>{
 
-  const fullname = req.body.fullname
+  const name = req.body.name
   const email = req.body.email
   const password = req.body.password
 
-    db.query(
-        "INSERT INTO userdb (name, email, password) VALUES (?,?,?)", 
-        [fullname, email, password], 
-        (err, results)=>{
-            if (err){
-                res.status(500).json({err})
-            }else{
-                res.status(201).json({results})
-            }
+    bcrypt.hash(password, saltRounds, (err, hash)=>{
+
+        if (err){
+            console.log(err)
+        }
+
+        db.query(
+            "INSERT INTO userdb (name, email, password) VALUES (?,?,?)", 
+            [name, email, hash], 
+            (err, results)=>{
+                 if (err){
+                     res.status(500).json({err})
+                 }else{
+                    res.status(201).json({results})
+                }
+            })
     })
 
 })
@@ -36,17 +71,23 @@ app.post('/login', (req, res)=>{
   const password = req.body.password
 
     db.query(
-        "SELECT * FROM userdb WHERE email = ? AND password = ?", 
-        [email, password], 
+        "SELECT * FROM userdb WHERE email = ?", 
+        email, 
         (err, results)=>{
             if (err){
                 res.status(500).json({err})
             }
             
-            if (results){
-                res.send(results)
+            if (results.length > 0){
+                bcrypt.compare(password, results[0].password, (error, response)=>{
+                    if (response){
+                        res.send(results)
+                    }else{
+                        res.send({message: "Wrong email/password combination"})
+                    }
+                })
             }else{
-                res.send({message: "Wrong email/password combination"})
+                res.send({message: "User does not exist"})
             }
     })
 })
