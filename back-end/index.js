09ae382624eb4b2b2b961cee4686a63dc1f165ad
Luke <lukebroadbent1996@gmail.com>
2021-06-16 17:04:23 +0100

    backend routes

--- back-end/index.js
+++ back-end/index.js
@@ -1,30 +1,25 @@
 const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
 const { response } = require('express');
-
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const register = require("./routes/registerRouter");
+const login = require("./routes/loginRouter")
+
+require('dotenv').config()
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST"],
-    credentials: true
-}));
+
 
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(session({
     key: "userId",
-    secret: "superdupertopsecret",
+    secret: process.env.secret,
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -32,75 +27,9 @@ app.use(session({
     } 
 }))
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'PASSWORD',
-    database: 'charitopia'
-})
 
-app.post('/register', (req, res)=>{
-
-  const name = req.body.name
-  const email = req.body.email
-  const password = req.body.password
-
-    bcrypt.hash(password, saltRounds, (err, hash)=>{
-
-        if (err){
-            console.log(err)
-        }
-
-        db.query(
-            "INSERT INTO userdb (name, email, password) VALUES (?,?,?)", 
-            [name, email, hash], 
-            (err, results)=>{
-                 if (err){
-                     res.status(500).json({err})
-                 }else{
-                    res.status(201).json({results})
-                }
-            })
-    })
-
-})
-
-app.get('/login', (req, res)=>{
-    if (req.session.user){
-        res.send({ loggedIn: true, user: req.session.user })
-    }else{
-        res.send({ loggedIn: false })
-    }
-})
-
-app.post('/login', (req, res)=>{
-
-  const email = req.body.email
-  const password = req.body.password
-
-    db.query(
-        "SELECT * FROM userdb WHERE email = ?", 
-        email, 
-        (err, results)=>{
-            if (err){
-                res.status(500).json({err})
-            }
-            
-            if (results.length > 0){
-                bcrypt.compare(password, results[0].password, (error, response)=>{
-                    if (response){
-                        req.session.user = results
-                        console.log(req.session.user)
-                        res.send(results)
-                    }else{
-                        res.send({message: "Wrong email/password combination"})
-                    }
-                })
-            }else{
-                res.send({message: "User does not exist"})
-            }
-    })
-})
+app.use( register);
+app.use( login);
 
 app.listen(3001, ()=>{
     console.log('Server running on port 3001')
---
+++ back-end/routes/registerRouter.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const mysql = require('mysql');
+const cors = require('cors');
+
+const router = express.Router();
+
+require('dotenv').config()
+
+const db = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database, 
+    port: process.env.port
+})
+
+router.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+
+router.post('/register', (req, res)=>{
+
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+  
+      bcrypt.hash(password, saltRounds, (err, hash)=>{
+  
+          if (err){
+              console.log(err)
+          }
+  
+          db.query(
+              "INSERT INTO userdb (name, email, password) VALUES (?,?,?)", 
+              [name, email, hash], 
+              (err, results)=>{
+                   if (err){
+                       res.status(500).json({err})
+                   }else{
+                      res.status(201).json({results})
+                  }
+              })
+      })
+  
+  })
+
+module.exports = router;
