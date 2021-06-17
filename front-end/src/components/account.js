6ec1b5031f4bd3389b2882e6655e1fee948fe343
Kim <faeriekim3@gmail.com>
2021-06-17 09:58:53 +0100

    fixed the issue of purple and turquoise text on all the pages instead of just the account page

--- front-end/src/components/account.js
+++ front-end/src/components/account.js
@@ -5,9 +5,9 @@ const Account = () => {
     return(
         <div id = "account_page_background">
             <h1>Account Details</h1>
-            <p>user ID: 34987345834</p>
-            <p>username: USERNAME</p>
-            <p>email: something@gmail.com</p>
+            <p className="account_text">user ID: 34987345834</p>
+            <p className="account_text">username: USERNAME</p>
+            <p className="account_text">email: something@gmail.com</p>
         </div>
     )
 }
