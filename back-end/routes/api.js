2b6cef5b9cd73c8b466b2a64a1dbd45f478f6fc9
Luke <lukebroadbent1996@gmail.com>
2021-06-22 11:39:13 +0100

    Api stored in backend

--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -4,6 +4,7 @@ const axios = require('axios')
 const rateLimit = require('express-rate-limit')
 const slowDown = require('express-slow-down')
 
+const getApi = require('./apifetch')
 
 
 //limits the amount of hits a ip can request
@@ -22,18 +23,19 @@ const speedLimiter = slowDown({
   
 });
 
-const baseURL = `https://api.data.charitynavigator.org/v2/Organizations?app_id=${process.env.API_ID}&app_key=${process.env.API_KEY}`
 
 let cachedData;
 let cachedTime;
 
-router.get('/', limiter, speedLimiter, async (req, res)=>{
-    // in memory cache so refreshing wont count as a hit
+router.post('/', limiter, speedLimiter, async (req, res)=>{
+  
+  // in memory cache so refreshing wont count as a hit
     if (cachedTime && cachedTime > Date.now() - 30 * 1000){
         return res.json(cachedData);
     }
    try{
-    const { data } = await axios.get(`${baseURL}`);
+    let search = req.body.search
+    const { data } = await axios.get(getApi(search));
 
     cachedData = data
     cachedTime = Date.now()
